f32f5a6753f7f6e3efa994839527461aabc374db
oliviakim217 <oliviakim217@users.noreply.github.com>
2022-04-25 16:00:45 -0400

    Initial setup

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# timestamp-microservice
-Timestamp Microservice
+
+# [Timestamp Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice)
---
+++ server.js
@@ -0,0 +1,25 @@
+// init project
+var express = require("express");
+var app = express();
+var cors = require("cors"); //enable CORS so that this API is remotely testable by FCC 
+
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(express.static("public"));
+
+// Routes
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: "hello API"});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
