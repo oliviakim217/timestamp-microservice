a6d8664aed85a6c38a2955fc77a1aaf7c1b0b053
oliviakim217 <oliviakim217@users.noreply.github.com>
2022-04-26 13:22:50 -0400

    Delete comments

--- server.js
+++ server.js
@@ -12,44 +12,32 @@ app.get("/", function (req, res) {
 });
 
 
-app.get("/api/:date_string?", function(req, res) {
+app.get("/api/:date_string", function(req, res) {
   
   const userInput = req.params.date_string;
 
   if (!userInput) {
     res.json({ unix: Math.round(Date.now()), utc: new Date().toUTCString() });
-  } else {
-    
-    function onlyNumbers(str) { // Check to see if the userInput only has numbers
-      return /^[0-9]+$/.test(str);
-    }
-    
-    if (userInput.includes("-")) {
-      var dateNumber = userInput.replace(/-/g, "");
-      dateNumber = Date.parse(userInput);
+  } else {      
+      if (userInput.includes("-")) {
+        var dateNumber = userInput.replace(/-/g, "");
+        dateNumber = Date.parse(userInput); 
+      } else {
+          var dateNumber = Number(userInput); //convert string to number
+        }
 
-    } else {
-        var dateNumber = Number(userInput); //convert string to number
+      const timestamp = new Date(dateNumber);
+      const UnixTime = Math.round(timestamp);
+      
+      if (timestamp.toUTCString() === "Invalid Date") {
+        res.json({ error: "Invalid Date" });
+      } else {
+        res.json({ unix: UnixTime, utc: timestamp.toUTCString() });
       }
-    // } else if (onlyNumbers(userInput)) {
-    //   var dateNumber = Number(userInput); //convert string to number
-    // }
-    // } else if ({
-    //   res.json({ error: "Invalid Date" });
-    // }
-
-    const timestamp = new Date(dateNumber);
-    const UnixTime = Math.round(timestamp);
-    
-    if (timestamp.toUTCString() === "Invalid Date") {
-      res.json({ error: "Invalid Date" });
-    } else {
-      res.json({ unix: UnixTime, utc: timestamp.toUTCString() });
     }
-  }
 }); 
 
 
 var listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+  console.log("The app is listening on port " + listener.address().port);
 });
